0f6d652a4a097c98f8d3924207c65129c5953be9
Daniel Larsson <dan1el_larss0n@protonmail.com>
2017-09-14 22:35:15 +0200

    move style from index.php to css.css

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML>
 <html>
 <head>
 	<title>Control robot</title>
@@ -109,18 +110,19 @@
 <body>
 
 	<div class="container">	
-		<div class="video_div" style="float:left; border-style:solid; border-width:5px; width:650px; height:490px; margin:0px; background-color:#C0C0C0">
+		<div class="video_div">
 		<video width="640" height="480" controls>
   			<source src="" type="video/mp4">
 			Your browser does not support the video tag.
 		</video> 
 		</div>
 
-		<div class="nickname_div" style="float:right; border-style:solid; border-width:5px 5px 5px 0px; margin:0px; width:490px; height:490px; background-color:#C0C0C0">
+		<div class="nickname_div">
 			<h1>Nicknames</h1>
+			<p>Placeholder for multiuser interface</p>
 		</div>
 
-		<div class="command_div clear" style="border-style:solid; border-width:0px 5px 5px 5px;">
+		<div class="command_div clear">
 			<h1>Keys</h1>
 			<div class="btn-group" role="group" aria-label="Basic example">
 				<button id="button_left" type="button" class="btn btn-secondary" style="font-size:100px">&larr;</button>
