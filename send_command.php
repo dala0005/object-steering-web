466eee10f9390ca480ed3f35509f3f198029c3ea
Daniel Larsson <dan1el_larss0n@protonmail.com>
2017-09-14 23:10:02 +0200

    change  to

--- send_command.php
+++ send_command.php
@@ -2,11 +2,10 @@
 /* check if data from index.php is sent */
 if(isset($_POST["key_value"]))
 {
-       
-        $robot_command = $_POST["key_value"];
+        $command_to_object = $_POST["key_value"];
 
        /* sending key_value to command.log */
-        $command = "echo " . $robot_command . " >> command.log";
+        $command = "echo " . $command_to_object . " >> command.log";
 
         /* execute */
         shell_exec($command); 
